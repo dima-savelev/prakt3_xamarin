23d40cfa6261bfc673b6c2717022937dcd3d274f
исп-41 <-41@LAB44-01>
2022-10-10 15:32:38 +0300

    Добавьте файлы проекта.

---
+++ app3/app3/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace app3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            await Navigation.PushModalAsync(new RegPage());
+            base.OnAppearing();
+        }
+
+        private void Exit_Clicked(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.GetCurrentProcess().CloseMainWindow();
+        }
+    }
+}
---
+++ app3/app3/RegPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace app3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegPage : ContentPage
+    {
+        public RegPage()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override bool OnBackButtonPressed()
+        {
+            return false;
+        }
+
+        private async void Login_Clicked(object sender, EventArgs e)
+        {
+            if (loginEntry.Text == null || passwordEntry.Text == null)
+            {
+                await DisplayAlert("Ошибка", "Введите логин и пароль", "Ok");
+                return;
+            }
+            if (loginEntry.Text.ToLower() == "admin" && passwordEntry.Text == "123")
+            {
+                await Navigation.PopModalAsync();
+            }
+            if (loginEntry.Text.ToLower() == "user" && passwordEntry.Text == "12345")
+            {
+                await Navigation.PopModalAsync();
+            }
+            else
+            {
+                await DisplayAlert("Ошибка", "Введенный логин или пароль неверны", "Ok");
+                return;
+            }
+        }
+
+        private void Exit_Clicked(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.GetCurrentProcess().CloseMainWindow();
+        }
+    }
+}
